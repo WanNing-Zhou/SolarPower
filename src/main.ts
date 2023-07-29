a570086582d7a49e1c8a2f0c6617cdc7c075c506
WangNing-Zhou <1879154660@qq.com>
2023-07-29 23:38:27 +0800

    feat: 添加element-plus图标库

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import App from './App.vue'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
 import router from "@/router/index.ts";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 const app =createApp(App)
+ //添加图标库
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
--- src/views/DataShow/components/PSCList.vue
+++ src/views/DataShow/components/PSCList.vue
@@ -1,43 +1,47 @@
 <template>
   <div class="PSC-list">
-    <el-table>
+    <el-table :data="tableData">
       <!--      用户录入-->
-      <el-table-column prop="name" label="关口表电量">
-        <template slot-scope="scope">
-          <el-input v-if="scope.row.edit" v-model="scope.row.name" placeholder="姓名"></el-input>
-          <span v-else>{{ scope.row.name }}</span>
+      <el-table-column prop="gatewayPower" label="关口表电量">
+        <template #default="scope">
+          <el-input v-if="scope.row.edit" v-model="scope.row.gatewayPower" placeholder="关口表电量"></el-input>
+          <span v-else>{{ scope.row.gatewayPower }}</span>
         </template>
       </el-table-column>
       <!--用户输入-->
-      <el-table-column prop="name" label="上网电量">
-        <template slot-scope="scope">
-          <el-input v-if="scope.row.edit" v-model="scope.row.name" placeholder="姓名"></el-input>
-          <span v-else>{{ scope.row.name }}</span>
+      <el-table-column prop="onlinePower" label="上网电量">
+        <template #default="scope">
+          <el-input v-if="scope.row.edit" v-model="scope.row.onlinePower" placeholder="上网电量"></el-input>
+          <span v-else>{{ scope.row.onlinePower }}</span>
         </template>
       </el-table-column>
 
-      <el-table-column prop="name" label="自用电量">
+      <el-table-column prop="selfUsePower" label="自用电量">
 
       </el-table-column>
 
-      <el-table-column prop="name" label="上网电费">
+      <el-table-column prop="onlineCharge" label="上网电费">
       </el-table-column>
 
-      <el-table-column prop="" label="自用电费">
+      <el-table-column prop="selfUseCharge" label="自用电费">
 
       </el-table-column>
 
       <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button v-if="scope.row.edit" type="text" size="medium" @click="confirmAdd(scope.row)">
+        <template #default="scope">
+          <el-button v-if="scope.row.edit" type="text" size="small" @click="confirmAdd(scope.row)">
             <i class="el-icon-check" aria-hidden="true"></i>
           </el-button>
           <div v-else>
-            <el-button type="text" size="medium" @click="editData(scope.row)">
-              <i class="el-icon-edit" aria-hidden="true"></i>
+            <el-button type="primary"  @click="editData(scope.row)">
+              <el-icon :size="20">
+                <Edit />
+              </el-icon>
             </el-button>
-            <el-button type="text" size="medium" @click="deleteData(scope.row,scope.$index)">
-              <i class="el-icon-delete" aria-hidden="true"></i>
+            <el-button type="primary" @click="deleteData(scope.row,scope.$index)">
+              <el-icon :size="20">
+                <Delete />
+              </el-icon>
             </el-button>
           </div>
         </template>
@@ -46,7 +50,7 @@
     </el-table>
 
 
-    <el-button type="text" @click="addData">添加数据</el-button>
+    <el-button type="primary" @click="addData">添加数据</el-button>
     <el-divider></el-divider>
     <el-button @click="submitData">提交 </el-button>
 
@@ -55,16 +59,35 @@
 
 <script setup lang="ts">
 
-import {reactive, ReactiveFlags, Ref, ref} from "vue";
+import {onMounted, reactive, ReactiveFlags, Ref, ref} from "vue";
 
 interface pscData{
   gatewayPower?:number | string //关口表电量
   onlinePower?: number | string // 上网电量
-  selfUseCharge ?: number | string //自用费用
-  onlineCahrge ?: number | string // 上网费用
+  selfUsePower ?: number | string //自用电量
+  onlineCharge ?: number | string // 上网费用
   selfUseCharge ?: number |  string // 自用电费
   edit ?: boolean //是否是编辑状态
 }
+
+const testData: Array<pscData> = [
+    {
+      gatewayPower : 134,
+      onlinePower : 13,
+      selfUsePower : 4455,
+      onlineCharge : 20,
+      selfUseCharge : 1030,
+      edit:false
+    },
+  {
+    gatewayPower : 1344,
+    onlinePower : 154,
+    selfUsePower : 44455,
+    onlineCharge : 250,
+    selfUseCharge : 51030,
+    edit:false
+  },
+]
 
 const tableData:Ref<Array<pscData>> = ref([{}])
 
@@ -73,6 +96,8 @@ const addData  = () => {
   tableData.value.push({
     edit: true,
   });
+  console.log('添加数据触发', tableData.value)
+
 }
 
 //确认添加
@@ -89,10 +114,15 @@ const editData = (row)=>{
 const deleteData = (row, index)=>{
   tableData.value.splice(index, 1);
 }
+
 //提交数据
 const submitData = ()=>{
-  alert(JSON.stringify(this.tableData))
+  alert(JSON.stringify(tableData.value))
 }
+
+onMounted(()=>{
+  tableData.value = testData;
+})
 
 </script>
 
