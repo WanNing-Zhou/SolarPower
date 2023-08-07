2a250bc95d1ceacbf6d8345bf8dbcadb1fcfccb3
WangNing-Zhou <1879154660@qq.com>
2023-08-07 22:35:49 +0800

    修改地址

--- src/views/DataShow/DataShow.vue
+++ src/views/DataShow/DataShow.vue
@@ -5,9 +5,9 @@
         <div class="header-left">
           <el-button link type="primary" @click="toLink('https://cn.fusionsolar.huawei.com/')">京东/富油/中站</el-button>
           <span> | </span>
-          <el-button link type="primary" @click="toLink(' https://www.ginlongcloud.com/#/station')">菲尔特</el-button>
+          <el-button link type="primary" @click="toLink('https://www.ginlongcloud.com/#/station')">菲尔特</el-button>
           <span> | </span>
-          <el-button link type="primary" @click="toLink('sems.com.cn')">三马架</el-button>
+          <el-button link type="primary" @click="toLink('https://www.sems.com.cn')">三马架</el-button>
           <span> | </span>
           <el-button link type="primary" @click="toLink('https://server-cn.growatt.com/login')">品柏/和旺/一厂/二厂</el-button>
 
