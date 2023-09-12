39bf76d60244822646c7826b3847dcf0d6f8dbfd
WangNing-Zhou <1879154660@qq.com>
2023-09-12 21:01:04 +0800

    fix: 修改侧边栏样式

--- src/components/SideLayout/SideLayout.vue
+++ src/components/SideLayout/SideLayout.vue
@@ -1,20 +1,54 @@
 <template>
   <div class="side-layout">
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick">
+<!--    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick">
       <template #default="{ node, data }">
-        <router-link :to="'/datashow/'+data.label">{{ node.label }}</router-link>
+        <div class="menu-item" @click="currentNodeId = node.id" :class="`${node.id == currentNodeId ? 'link-active' : ''}`">
+          <router-link :to="'/datashow/'+data.label">{{ node.label }}</router-link>
+        </div>
       </template>
-    </el-tree>
+    </el-tree>-->
+
+    <el-menu
+        default-active="C1"
+
+        @open="handleOpen"
+        @close="handleClose"
+    >
+      <template v-for="item in data">
+        <el-sub-menu :index="item.id">
+          <template #title>
+            <el-menu-item :index="item.id">
+<!--              <el-icon><location /></el-icon>-->
+              <router-link class="menu-item" :to="'/datashow/'+item.label">{{ item.label }}</router-link>
+            </el-menu-item>
+          </template>
+
+          <template v-for="cItem in item.children">
+            <el-menu-item :index="cItem.id">
+              <router-link class="menu-item" :to="'/datashow/'+cItem.label">{{ cItem.label }}</router-link>
+            </el-menu-item>
+          </template>
+
+        </el-sub-menu>
+      </template>
+
+
+    </el-menu>
+
   </div>
 </template>
 
 <script setup lang="ts">
+
+import {ref} from "vue";
 
 interface Tree {
   label: string
   id?: string
   children?: Tree[]
 }
+
+const currentNodeId = ref('C1');
 
 const handleNodeClick = (data: Tree) => {
   // console.log(data)
@@ -48,6 +82,13 @@ const defaultProps = {
 }
 
 
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+
 </script>
 
 <style lang="scss" scoped>
@@ -57,10 +98,17 @@ const defaultProps = {
   background-color: #f9f9f9;
   overflow-y: auto;
 
-  .el-tree {
+  :deep(.el-tree) {
     background-color: transparent;
     user-select: none;
   }
+
+
+  .menu-item{
+    line-height: 40px;
+    margin: 4px 0;
+  }
+
 
 }
 </style>
