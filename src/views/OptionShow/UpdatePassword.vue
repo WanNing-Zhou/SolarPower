a6b84567ada11e156b6473c11be6da210a80c099
WangNing-Zhou <1879154660@qq.com>
2024-05-31 14:58:11 +0800

    feat:  添加表单校验规则

--- src/views/OptionShow/UpdatePassword.vue
+++ src/views/OptionShow/UpdatePassword.vue
@@ -1,14 +1,14 @@
 <template>
   <el-dialog v-model="vis" title="修改密码" width="500">
     <div>
-      <el-form label-width="120">
-        <el-form-item label="旧密码">
+      <el-form ref="formRef" :rules="rules" label-width="120">
+        <el-form-item prop="oldPassword" label="旧密码">
           <el-input v-model="form.oldPassword" show-password type="password"></el-input>
         </el-form-item>
-        <el-form-item label="新密码">
+        <el-form-item prop="newPassword" label="新密码">
           <el-input v-model="form.newPassword" show-password type="password"></el-input>
         </el-form-item>
-        <el-form-item label="确认密码">
+        <el-form-item prop="rePassword" label="确认密码">
           <el-input v-model="form.rePassword" show-password type="password"></el-input>
         </el-form-item>
       </el-form>
@@ -21,8 +21,8 @@
 
 <script setup lang="ts">
 
-import {computed, ref} from "vue";
-import {ElMessage} from "element-plus";
+import {computed, reactive, ref} from "vue";
+import {ElMessage, FormInstance, FormRules} from "element-plus";
 import {updatePassword} from "@/api/user.ts";
 
 type Prop = {
@@ -31,6 +31,19 @@ type Prop = {
 
 const props = defineProps<Prop>()
 const emits = defineEmits(['update:visible'])
+
+const rules = reactive<FormRules>({
+  oldPassword: [
+    {min: 6 ,max: 32, message: '密码长度应在6-32间'}
+  ],
+  newPassword: [
+    {min: 6 ,max: 32, message: '密码长度应在6-32间'}
+  ],
+  rePassword: [
+    {min: 6 ,max: 32, message: '密码长度应在6-32间'}
+  ]
+})
+const formRef = ref<FormInstance>()
 const vis = computed({
   get() {
     return props.visible
@@ -48,23 +61,29 @@ const form = ref({
 
 // 提交表单
 const confirmHandle  = async () =>{
-  const {oldPassword, newPassword, rePassword} = form.value
-  if(!newPassword || !oldPassword){
-    ElMessage.warning('密码不能为空')
-    return
-  }
-  if(newPassword != rePassword){
-    ElMessage.warning('新密码两次输入不一致')
-    return
-  }
+  formRef.value?.validate((async (isValid) => {
+    if(isValid){
 
-  const res: any = await updatePassword({oldPassword, newPassword})
-  if(res.code == 200){
-    ElMessage.success('修改成功')
-    // 关闭dialog
-    emits('update:visible', false)
-  }
+      const {oldPassword, newPassword, rePassword} = form.value
+      if(!newPassword || !oldPassword){
+        ElMessage.warning('密码不能为空')
+        return
+      }
+      if(newPassword != rePassword){
+        ElMessage.warning('新密码两次输入不一致')
+        return
+      }
 
+      const res: any = await updatePassword({oldPassword, newPassword})
+      if(res.code == 200){
+        ElMessage.success('修改成功')
+        // 关闭dialog
+        emits('update:visible', false)
+      }
+    }else {
+      ElMessage.warning('表单校验失败, 请检查表单')
+    }
+  }))
 }
 
 </script>
