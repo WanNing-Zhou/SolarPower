9b7328bc3ec8f70d292fc0044ad00d248235f83c
buildgods <15564595518@163.com>
2024-06-18 16:25:14 +0800

    合并登录页面

---
+++ dist/assets/user-8ed530fc.js
@@ -0,0 +1,1 @@
+import{B as o}from"./index-fd38cc11.js";function s(t){return o("/api/user/login",{method:"post",data:t})}function e(){return o.get("/api/user/info")}function u(t){return o.post("/api/user/changePassword",t)}function n(){return o.post("/api/user/logout")}export{u as a,e as g,s as l,n as u};
